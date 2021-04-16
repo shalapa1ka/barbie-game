c50b4551e2749042bde8b8172e975e38e8c54567
shalapa1ka <flecks07@gmail.com>
2021-04-16 14:21:35 +0300

    make *hero* | walk + jump

--- main.py
+++ main.py
@@ -11,15 +11,24 @@ WHITE = (255, 255, 255)
 BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
-# *****
+# ***** hero params
+x = W // 2
+y = H // 2
+speed = 4
+jump = 20
+move = jump + 1
+# ***** draw main scene
 screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption('my game')
-screen.fill(BLACK)
+ground = pygame.Surface((W, 100))
+hero = pygame.Surface((50, 50))
+hero.fill(WHITE)
+ground.fill(GREEN)
+rect_ground = ground.get_rect(bottom=H)
+rect_hero = hero.get_rect(centerx=x - 25, bottom=H - ground.get_height())
 clock = pygame.time.Clock()
-
-x = W // 2  # obj pos_x
-y = H // 2  # obj pos_y
-speed = 4  # obj speed
+screen.blit(ground, rect_ground)
+screen.blit(hero, rect_hero)
 
 # isRun = True
 while True:  # isRun:
@@ -28,28 +37,27 @@ while True:  # isRun:
             exit()
             # pygame.quit()
             # isRun = False
-        mouse_btn = pygame.mouse.get_pressed(3)
-        if mouse_btn[0] == 1:
-            x, y = pygame.mouse.get_pos()
-            r = 2
-            pygame.draw.circle(screen, WHITE, (x - (r // 2), y - (r // 2)), r)
-        if mouse_btn[2] == 1:
-            screen.fill(BLACK)
-
-    #         x = pygame.mouse.get_pos()[0] - 25
-    #         y = pygame.mouse.get_pos()[1] - 25
-    # keys = pygame.key.get_pressed()
-
-    # if keys[pygame.K_LEFT]:
-    #     x -= speed
-    # if keys[pygame.K_RIGHT]:
-    #     x += speed
-    # if keys[pygame.K_UP]:
-    #     y -= speed
-    # if keys[pygame.K_DOWN]:
-    #     y += speed
-
-    # pygame.draw.rect(screen, BLACK, (x, y, 50, 50))
+    keys = pygame.key.get_pressed()
+    # ***** walk
+    if keys[pygame.K_LEFT]:
+        rect_hero.left -= speed
+    if keys[pygame.K_RIGHT]:
+        rect_hero.left += speed
+    # *****
+    # ***** jump
+    if keys[pygame.K_SPACE] and H - ground.get_height() == rect_hero.bottom:
+        move = -jump
+    if move <= jump:
+        if rect_hero.bottom + move < H - ground.get_height():
+            rect_hero.bottom += move
+            if move < jump:
+                move += 1
+        else:
+            rect_hero.bottom = H - ground.get_height()
+            move = jump + 1
+    # *****
+    screen.fill(BLACK)
+    screen.blit(ground, rect_ground)
+    screen.blit(hero, rect_hero)
     pygame.display.update()
     clock.tick(FPS)
-    print('test')
