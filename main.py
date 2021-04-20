a9c31a42788fb916dcd8d6b3f67f3728f998dd46
shalapa1ka <flecks07@gmail.com>
2021-04-20 15:34:26 +0300

    added:  func collideBalls  rename Mario to hero

--- main.py
+++ main.py
@@ -15,7 +15,6 @@ BLUE = (0, 0, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 
-
 # ***** draw main scene
 screen = pygame.display.set_mode((W, H))
 pygame.display.set_caption('Блядки')
@@ -27,7 +26,7 @@ ground = pygame.image.load('images/dirt.jpg').convert()
 ground = pygame.transform.scale(ground, (W, 100))
 rect_ground = ground.get_rect(bottom=H)
 # create hero
-Mario = Hero(W // 2, H - 100, 5, 'barbie.png')
+hero = Hero(W // 2, H - 100, 5, 'barbie.png')
 
 # create balls group
 balls = pygame.sprite.Group()
@@ -37,14 +36,22 @@ def createBall(group):
     x = randint(20, W - 20)
     speed = randint(1, 3)
     surf = pygame.image.load('images/ken.png').convert_alpha()
-    surf = pygame.transform.scale(surf, (100, 276)) # 172x470
+    surf = pygame.transform.scale(surf, (100, 276))  # 172x470
     return Ball(x, speed, surf, group)
 
 
-createBall(balls)
+game_score = 0
+
+
+def collideBalls():
+    global game_score
+    for ball in balls:
+        if hero.rect.collidepoint(ball.rect.center):
+            game_score += 100
+            ball.kill()
+
 
 clock = pygame.time.Clock()
-
 
 # isRun = True
 while True:  # isRun:
@@ -58,12 +65,15 @@ while True:  # isRun:
     keys = pygame.key.get_pressed()
     mouse_click = pygame.mouse.get_pressed(3)
 
-    screen.blit(sky, (0, 0))
-    screen.blit(ground, rect_ground)
-    Mario.draw(screen)
-    Mario.move(keys)
-    Mario.jump(keys, rect_ground)
+    hero.draw(screen)
+    hero.move(keys)
+    hero.jump(keys, rect_ground)
     balls.draw(screen)
     balls.update(H)
+    collideBalls()
+
+    screen.blit(sky, (0, 0))
+    screen.blit(ground, rect_ground)
+
     pygame.display.update()
     clock.tick(FPS)
