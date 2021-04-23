ee1f5fe2ad30f02fe7b699b01dd46aa7875765fa
shalapa1ka <flecks07@gmail.com>
2021-04-23 17:41:30 +0300

    class inputBox done

--- input.py
+++ input.py
@@ -7,14 +7,46 @@ FONT = pg.font.SysFont('arial', 30, True)
 
 
 class InputBox:
-    def __init__(self):
-        pass
+    def __init__(self, x, y, w, h, text='noname'):
+        self.rect = pg.Rect(x, y, w, h)
+        self.color = COLOR_INACTIVE
+        self.text = text
+        self.txt_surface = FONT.render(text, True, self.color)
+        self.active = False
 
     def handle_event(self, event):
-        pass
+        if event.type == pg.MOUSEBUTTONDOWN:
+            # If the user clicked on the input_box rect.
+            if self.rect.collidepoint(event.pos):
+                # Toggle the active variable.
+                self.active = not self.active
+            else:
+                self.active = False
+            # Change the current color of the input box.
+            self.color = COLOR_ACTIVE if self.active else COLOR_INACTIVE
+        if event.type == pg.KEYDOWN:
+            if self.active:
+                if event.key == pg.K_RETURN:
+                    print(self.text)
+                    self.text = ''
+                elif event.key == pg.K_BACKSPACE:
+                    self.text = self.text[:-1]
+                else:
+                    self.text += event.unicode
+                # Re-render the text.
+                self.txt_surface = FONT.render(self.text, True, self.color)
 
-    def update(self):
-        pass
+    def get_name(self):
+        return self.text
+
+    def update(self, screen):
+        # Resize the box if the text is too long.
+        width = max(200, self.txt_surface.get_width() + 10)
+        self.rect.w = width
+        self.rect.x = screen.get_rect().width // 2 - self.rect.w // 2
 
     def draw(self, screen):
-        pass
+        # Blit the text.
+        screen.blit(self.txt_surface, (self.rect.x + 5, self.rect.y + 5))
+        # Blit the rect.
+        pg.draw.rect(screen, self.color, self.rect, 2)
