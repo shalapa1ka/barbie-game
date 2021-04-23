be90ac5c00155e5f0af40e12e71aa8373f98e92c
shalapa1ka <flecks07@gmail.com>
2021-04-23 16:46:55 +0300

    added new inputBox class (empty) #TODO fill the func

---
+++ input.py
@@ -0,0 +1,20 @@
+import pygame as pg
+
+pg.init()
+COLOR_INACTIVE = pg.Color('lightskyblue3')
+COLOR_ACTIVE = pg.Color('dodgerblue2')
+FONT = pg.font.SysFont('arial', 30, True)
+
+
+class InputBox:
+    def __init__(self):
+        pass
+
+    def handle_event(self, event):
+        pass
+
+    def update(self):
+        pass
+
+    def draw(self, screen):
+        pass
